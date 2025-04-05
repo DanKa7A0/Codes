25d8234403cd50f4aa4ae35b9f0b3fa754e259b1
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-04-05 15:59:10 +0300

    implement stamp creation form with validation and submission logic

--- 04. Applications/09.Exam/src/views/stamp/create.js
+++ 04. Applications/09.Exam/src/views/stamp/create.js
@@ -1,4 +1,7 @@
+import page from '../../../node_modules/page/page.mjs';
 import { render, html } from '../../../node_modules/lit-html/lit-html.js';
+import { post } from '../../api/api.js';
+import { setUserData } from '../../utility/local storage.js';
 
 export default function() {
     render(CreateTemplate(), document.querySelector('main'));
@@ -10,7 +13,7 @@ function CreateTemplate(){
       <section id="create">
         <div class="form">
           <h2>Add Post Stamp</h2>
-          <form class="create-form">
+          <form class="create-form" @submit=${SumbitCreateStamp}>
             <input type="text" name="name-input" id="name-input" placeholder="Stamp Name" />
             <input type="text" name="image-url-input" id="image-url-input" placeholder="Image URL" />
             <input type="number" id="year-input" name="year-input" placeholder="year" />
@@ -20,4 +23,26 @@ function CreateTemplate(){
         </div>
       </section>
     `;
+}
+
+async function SumbitCreateStamp(e){
+    try {
+        e.preventDefault();
+        const data = Object.fromEntries(new FormData(e.currentTarget));
+        
+        if (data["image-url-input"] === "" || data["more-info-textarea"] === "" || data["name-input"] === "" || data["year-input"] === "") {
+            alert("All fields are required!");
+            return 0;
+        }
+        
+        const sendData = {
+            name: data["name-input"],
+            imageUrl: data["image-url-input"],
+            year: Number(data["year-input"]),
+            learnMore: data["more-info-textarea"],
+        };
+        await post("/data/stamps", sendData);
+        page.redirect("/dashboard");
+        
+    } catch (error) {}
 }
