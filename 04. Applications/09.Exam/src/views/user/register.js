c8cee74206a70afe6850ab01bb4a77d7048cbf0a
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-04-05 15:09:00 +0300

    implement registration form with validation and user redirection

--- 04. Applications/09.Exam/src/views/user/register.js
+++ 04. Applications/09.Exam/src/views/user/register.js
@@ -1,4 +1,8 @@
+import page from '../../../node_modules/page/page.mjs';
 import { render, html } from '../../../node_modules/lit-html/lit-html.js';
+import { post } from '../../api/api.js';
+import { setUserData } from '../../utility/local storage.js';
+
 
 export default function() {
     render(RegisterTemplate(), document.querySelector('main'));
@@ -10,7 +14,7 @@ function RegisterTemplate(){
         <section id="register">
             <div class="form">
             <h2>Register</h2>
-            <form class="register-form">
+            <form class="register-form" @submit=${SubmitRegister}>
                 <input type="text" name="email" id="register-email" placeholder="email" />
                 <input type="password" name="password" id="register-password" placeholder="password" />
                 <input type="password" name="re-password" id="repeat-password" placeholder="repeat password" />
@@ -20,4 +24,27 @@ function RegisterTemplate(){
             </div>
         </section>
     `
+}
+
+async function SubmitRegister(e){
+    try {
+        e.preventDefault();
+        const data = Object.fromEntries(new FormData(e.currentTarget));
+        console.log(data);
+        
+        if (data.email === "" || data.password === "" || data["re-password"] === "") {
+            alert("All fields are required!");
+            return 0;
+        }
+
+        if (data.password !== data["re-password"]) {
+            alert("Passwords do not match!");
+            return 0;
+        }
+
+        const response = await post("/users/register", data);
+        setUserData(response);
+        page.redirect("/");
+        
+    } catch (error) {}
 }
