8801c78353fcc0a9375688996f18df1e0f44094b
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-04-05 14:45:32 +0300

    implement login functionality with form validation and user redirection

--- 04. Applications/09.Exam/src/views/user/login.js
+++ 04. Applications/09.Exam/src/views/user/login.js
@@ -1,4 +1,8 @@
+import page from '../../../node_modules/page/page.mjs';
 import { render, html } from '../../../node_modules/lit-html/lit-html.js';
+import { post } from '../../api/api.js';
+import { setUserData } from '../../utility/local storage.js';
+
 
 export default function() {
     render(LoginTemplate(), document.querySelector('main'));
@@ -10,15 +14,33 @@ function LoginTemplate(){
         <section id="login">
             <div class="form">
             <h2>Login</h2>
-            <form class="login-form">
+            <form class="login-form" @submit=${SubmitLogin}>
                 <input type="text" name="email" id="email" placeholder="email" />
                 <input type="password" name="password" id="password" placeholder="password" />
                 <button type="submit">login</button>
                 <p class="message">
-                Not registered? <a href="#">Create an account</a>
+                    Not registered? <a href="/register">Create an account</a>
                 </p>
             </form>
             </div>
         </section>
     `
+}
+
+async function SubmitLogin(e){
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.currentTarget));
+
+    if (data.email === "" || data.password === "") {
+        alert("All fields are required!");
+        return 0;
+    }
+
+    const response = await post("/users/login", data);
+    if (response.accessToken) {
+        setUserData(response);
+        page.redirect("/dashboard");
+    } else {
+        alert("Invalid email or password!");
+    }   
 }
