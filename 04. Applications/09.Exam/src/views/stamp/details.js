77fa095d3d9fc62a5bffde4be9fe9d9550cff30a
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-04-05 16:19:19 +0300

    refactor: update stamp details rendering to fetch data dynamically

--- 04. Applications/09.Exam/src/views/stamp/details.js
+++ 04. Applications/09.Exam/src/views/stamp/details.js
@@ -1,40 +1,48 @@
 import { render, html } from '../../../node_modules/lit-html/lit-html.js';
+import { get } from '../../api/api.js';
+import { getUserData } from '../../utility/local storage.js';
 
-export default function() {
-    render(DetailsTemplate(), document.querySelector('main'));
+export default async function(ctx) {
+    try {
+        const response = await get("/data/stamps/" + ctx.params.id);
+        console.log(response);
+        render(DetailsTemplate(response), document.querySelector('main'));
+    }
+    catch (error) {}
 }
 
-function DetailsTemplate(){
+function DetailsTemplate(row){
     return html`
         <!-- Details page -->
         <section id="details">
             <div id="details-wrapper">
-            <img id="details-img" src="./images/France 1858.webp" alt="example1" />
+            <img id="details-img" src="${row.imageUrl}" alt="example1" />
             <div>
-                <p id="details-name">Bordeaux 40c</p>
+                <p id="details-name">${row.name}</p>
                 <div id="info-wrapper">
-                <div id="details-year-description">
-                    <p id="year-description">
-                    Year of oldest stamps - <span id="year">1870</span> 
-                    </p>
-                    <p id="more-info">
-                    France 1870 - Yvert & Tellier No. 48 - Bordeaux Issue - 40 centimes Orange
-                    This is a 40 centimes orange stamp from the Bordeaux issue, released during the Franco-Prussian War in
-                    1870. Printed under emergency conditions, Bordeaux issue stamps are known for their crude yet
-                    historically significant production.
-                    Mint Condition (new), with original gum.
-                    </p>
-                </div>
+                    <div id="details-year-description">
+                        <p id="year-description">
+                        Year of oldest stamps - <span id="year">${row.year}</span> 
+                        </p>
+                        <p id="more-info">${row.learnMore}</p>
+                    </div>
                 </div>
                 <h3>Stamp total likes:<span id="likes">0</span></h3>
 
                 <!--Edit and Delete are only for creator-->
                 <div id="action-buttons">
-                <a href="#" id="edit-btn">Edit</a>
-                <a href="#" id="delete-btn">Delete</a>
-
-                <!--Bonus - Only for logged-in users ( not authors )-->
-                <a href="#" id="like-btn">Like</a>
+                    ${
+                        row._ownerId === getUserData()?._id
+                            ?
+                                html`
+                                    <a href="/edit/${row._id}" id="edit-btn">Edit</a>
+                                    <a href="/delete/${row._id}" id="delete-btn">Delete</a>
+                                ` 
+                            :
+                                html`
+                                    <a href="#" id="like-btn">Like</a>
+                                `
+                    }                
                 </div>
             </div>
             </div>
