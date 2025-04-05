400f1176030994c6e888b420beff3a0863ebfe5c
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-04-05 15:43:49 +0300

    refactor: update dashboard to fetch and display stamps dynamically

--- 04. Applications/09.Exam/src/views/stamp/dashboard.js
+++ 04. Applications/09.Exam/src/views/stamp/dashboard.js
@@ -1,47 +1,38 @@
 import { render, html } from '../../../node_modules/lit-html/lit-html.js';
+import { get } from '../../api/api.js';
 
-export default function() {
-    render(DashboardTemplate(), document.querySelector('main'));
+export default async function() {
+    try {        
+        const response = await get("/data/stamps?sortBy=_createdOn%20desc");
+        render(DashboardTemplate(response), document.querySelector('main')); 
+    } catch (error) {}
 }
 
-function DashboardTemplate(){
+function DashboardTemplate(data) {
     return html`
         <!-- Dashboard page -->
         <h2>Collection</h2>
         <section id="collection">
             <!-- Display a div with information about every post (if any)-->
-            <div class="stamp">
-            <img src="./images/Austria 1858.webp" alt="example1" />
-            <div class="stamp-info">
-                <h3 class="name">KK Blue</h3>
-                <p class="year-description">
-                Year of oldest stamps - <span class="year">1858</span> 
-                </p>
-                <a class="learn-more-btn" href="#">Learn More</a>
-            </div>
-            </div>
-            <div class="stamp">
-            <img src="./images/France 1858.webp" alt="example2" />
-            <div class="stamp-info">
-                <h3 class="name">Bordeaux 40c</h3>
-                <p class="year-description">
-                Year of oldest stamps - <span class="year">1870</span> 
-                </p>
-                <a class="learn-more-btn" href="">Learn More</a>
-            </div>
-            </div>
-            <div class="stamp">
-            <img src="./images/Austria 1945.webp" alt="example3" />
-            <div class="stamp-info">
-                <h3 class="name">Losenstein 5Pf</h3>
-                <p class="year-description">
-                Year of oldest stamps - <span class="year">1945</span> 
-                </p>
-                <a class="learn-more-btn" href="#">Learn More</a>
-            </div>
-            </div>
-            <!-- Display an h2 if there are no posts -->
-        <h2 id="no-stamp">No Stamps Added.</h2>
+            ${  
+                data.length == 0 
+                ? html`<h2>>No Stamps Added.</h2>`  
+                : data.map(row => StampTemplate(row))
+            }            
         </section>     
+    `;
+}
+function StampTemplate(row) {
+    return html`
+         <div class="stamp">
+            <img src="${row.imageUrl}" alt="example1" />
+            <div class="stamp-info">
+                <h3 class="name">${row.name}</h3>
+                <p class="year-description">
+                Year of oldest stamps - <span class="year">${row.year}</span> 
+                </p>
+                <a class="learn-more-btn" href="/details/${row._id}">Learn More</a>
+            </div>                
+        </div>
     `;
 }
