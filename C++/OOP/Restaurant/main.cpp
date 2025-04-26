f5e41169267b3f370bd2fa97dbe0f70372b366e5
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-04-26 14:44:38 +0300

    Implement Discount:  Percentage and Flat , and integrate into Order class and main

--- C++/OOP/Restaurant/main.cpp
+++ C++/OOP/Restaurant/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #include "order.h"
-
+#include "discount.h"
 
 int main(int argc, char const *argv[]){
     Order order;
@@ -19,6 +19,24 @@ int main(int argc, char const *argv[]){
             std::cin >> name >> price;
             Item item(name, price);
             order.addItem(item);
+            continue;
+        }
+
+        if (command == "discount") {
+            std::string type;
+            double discountValue;
+            std::cin >> type >> discountValue;
+
+            if (type == "percentage") {
+                order.setDiscount(new Percentage(discountValue));
+                continue;
+            }
+            if (type == "flat") {
+                order.setDiscount(new Flat(discountValue));
+                continue;
+            }
+
+            std::cout << "Unknown discount type: " << type << std::endl;            
             continue;
         }
 
--- C++/OOP/Restaurant/order.cpp
+++ C++/OOP/Restaurant/order.cpp
@@ -14,10 +14,20 @@ void Order::printOrder(std::ostream &out) const {
         out << "Item: " << item.getName() << ", Price: " << item.getPrice() << std::endl;
         totalSum += item.getPrice();
     }
+
+    if (discount) {
+        discount->print(std::cout);
+        totalSum = discount->apply(totalSum);
+    }
+
     out << "Total Sum: " << totalSum << std::endl;
 }
 
 void Order::printWelcomeMessage(std::ostream &out) const {
     out << "Welcome to the restaurant!" << std::endl;
     out << "Please place your order." << std::endl;
+}
+
+void Order::setDiscount(Discount *discount) {
+    this->discount = discount;
 }
