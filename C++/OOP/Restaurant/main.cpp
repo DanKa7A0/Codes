7ede805f6aeb09158761e6605dd9fd3e56a85d17
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-04-26 13:36:43 +0300

    main: user interaction

--- C++/OOP/Restaurant/main.cpp
+++ C++/OOP/Restaurant/main.cpp
@@ -1,8 +1,34 @@
+#include <iostream>
+
 #include "order.h"
+
 
 int main(int argc, char const *argv[]){
     Order order;
+    order.printWelcomeMessage(std::cout);
 
+    std::string command;    
+    while (std::cin >> command) {
+        if (command == "end") {
+            break;
+        }
+
+        if (command == "add") {
+            std::string name;
+            double price;
+            std::cin >> name >> price;
+            Item item(name, price);
+            order.addItem(item);
+            continue;
+        }
+
+        if (command == "print") {
+            order.printOrder(std::cout);
+            continue;
+        }
+
+        std::cout << "Unknown command: " << command << std::endl;    
+    }
 
     return 0;
 }
