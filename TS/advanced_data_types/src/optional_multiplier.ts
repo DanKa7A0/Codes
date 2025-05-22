b856e6b1c95a4f8caf5b66d2c2662b352e940685
DANI-PC <daniel.bratov@unicontsoft.com>
2025-05-23 00:05:23 +0300

    feat:  addед OptionalMultiplier2 implementation which has a simple solution

--- TS/advanced_data_types/src/optional_multiplier.ts
+++ TS/advanced_data_types/src/optional_multiplier.ts
@@ -3,7 +3,6 @@ function ConvertNumber(num: number|string): number {
     if (typeof num === "string") num = parseInt(num);
     return num;
 }
-
 function OptionalMultiplier(num1?: number|string, num2?: number|string, num3?: number|string): number {
     let result: number = 1;
     result *= ConvertNumber(num1);
@@ -13,8 +12,24 @@ function OptionalMultiplier(num1?: number|string, num2?: number|string, num3?: n
     return result;
 }
 
-console.log(OptionalMultiplier('3', 5, '10' ));
-console.log(OptionalMultiplier('2','2' ));
-console.log(OptionalMultiplier(undefined, 2, 3));
-console.log(OptionalMultiplier(7, undefined, '2'));
-console.log(OptionalMultiplier());
+// console.log(OptionalMultiplier('3', 5, '10' ));
+// console.log(OptionalMultiplier('2','2' ));
+// console.log(OptionalMultiplier(undefined, 2, 3));
+// console.log(OptionalMultiplier(7, undefined, '2'));
+// console.log(OptionalMultiplier());
+
+
+
+function OptionalMultiplier2(num1?: number|string, num2?: number|string, num3?: number|string): number {
+    num1 = num1 != null ? Number(num1) : 1;
+    num2 = num2 != null ? Number(num2) : 1;
+    num3 = num3 != null ? Number(num3) : 1;
+
+    return num1 * num2 * num3;
+}
+
+console.log(OptionalMultiplier2('3', 5, '10' ));
+console.log(OptionalMultiplier2('2','2', null));
+console.log(OptionalMultiplier2(undefined, 2, 3));
+console.log(OptionalMultiplier2(7, undefined, '2'));
+console.log(OptionalMultiplier2());
