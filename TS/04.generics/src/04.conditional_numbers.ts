b197b7b7a9e9655f794d95192fbab1ebe5c60151
DANI-PC <daniel.bratov@unicontsoft.com>
2025-06-06 23:05:31 +0300

    feat: implement conditionalNumber function for type-safe number handling

---
+++ TS/04.generics/src/04.conditional_numbers.ts
@@ -0,0 +1,19 @@
+function conditionalNumber<T>(input: T extends number ? T : string){
+    if (typeof input === "number") {
+        console.log(input.toFixed(2));
+        return;    
+    } 
+    
+    console.log(input);
+    
+}
+
+
+
+
+conditionalNumber<number>(20.3555);
+conditionalNumber<string>('wow');
+conditionalNumber<boolean>('a string');
+
+// conditionalNumber<boolean>(30); // TS error
+// conditionalNumber<number>('test'); // TS error
